affe4e848494ef804327d7c5f146a2abcd2fab08
Suborna Nath <subornadnath@gmail.com>
2022-12-16 20:02:00 -0500

    added input argument for cash

--- application.py
+++ application.py
@@ -423,7 +423,7 @@ def register():
                 return errorPage(title="Forbidden", info = "Username already taken", file="animated-403.svg")
         
         # All users automatically recieve $10,000 to start with
-        cash = 10000
+        cash = request.form.get("cash")
 
         # Add and commit the data into database
         db.session.add(Users(username, hashed, cash))
